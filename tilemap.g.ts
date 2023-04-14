3ff7fe6b1f635b0aabcdf7852fece61445bbd576
DarshanG62023 <130467366+DarshanG62023@users.noreply.github.com>
2023-04-14 19:01:23 +0530

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,11 +1,11 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile0 = image.ofBuffer(hex``);
+    export const transparency16 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "tile0":return tile0;
+            case "transparency16":return transparency16;
         }
         return null;
     })
